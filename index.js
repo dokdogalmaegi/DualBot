ad2cc2e6637460036894c9b21f7725718dfbbe1f
임준영 <clik202012@gmail.com>
2022-04-10 00:16:32 +0900

    init Project

---
+++ index.js
@@ -0,0 +1,17 @@
+const OAuth = require('./OAuth');
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+client.on('ready', () => {
+  console.log(`I am Ready ${client.user.tag}`);
+});
+
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isCommand()) return;
+    
+  if (interaction.commandName === 'ping') {
+    await interaction.reply('Pong!');
+  }
+});
+
+client.login(OAuth.token);
